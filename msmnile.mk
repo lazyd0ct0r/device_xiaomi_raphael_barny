ecaf44d29c25e49d8698a5125db03a71a147f951
David Sehr <sehr@google.com>
2020-04-25 15:52:19 -0700

    raphael: Use dex2oat64 on coral/flame
    Enable dex2oat64 usage on coral/flame.
    
    Bug: 153380900
    Test: boot and install an application.
    Change-Id: Ifff726eb2f8bd315a42c02bdca9ff10cf027a06c
    (cherry picked from commit 5bb0457da144d6740ba74614482b4e8ee9bbba4c)

--- msmnile.mk
+++ msmnile.mk
@@ -518,3 +518,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # Override heap growth limit due to high display density on device
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m
+
+# Use 64-bit dex2oat for better dexopt time.
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat64.enabled=true
