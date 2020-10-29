1db496b340405fcf4c07662d7021fd0997f95112
Adhitya Mohan <me@adhityamohan.in>
2020-10-29 10:54:29 +0530

    raphael: Build librmnetctl.so

--- device.mk
+++ device.mk
@@ -428,7 +428,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjson \
     libril \
-    librilutils
+    librilutils \
+    librmnetctl
 
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite-vendorcompat \
